cb4cc63413f37c9783d8fe75642beb4110a2e61d
paliu8908 <73498620+paliu8908@users.noreply.github.com>
2024-11-04 22:57:29 -0500

    patrick.py

--- patrick.py
+++ patrick.py
@@ -3,3 +3,9 @@ import numpy as np
 import numpy_financial as npf
 import pandas as pd
 
+
+
+#short by sharpe consumes a dataframe of stocks from the same industry and their daily prices prices
+#and produces a sorted dataframe by their sharpe ratio.
+def sort_by_sharpe(dataframe):
+  
