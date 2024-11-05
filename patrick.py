d07d2c941abb7bf62c932c4d493f45a7b9b757e1
paliu8908 <73498620+paliu8908@users.noreply.github.com>
2024-11-04 23:25:43 -0500

    Update patrick.py

--- patrick.py
+++ patrick.py
@@ -5,7 +5,17 @@ import pandas as pd
 
 
 
-#short by sharpe consumes a dataframe of stocks from the same industry and their daily prices prices
+#ticker_prices consumes a list of stocks, start date, end date, and an interval
+# and produces a dataframe of prices for each of the stock
+def ticker_prices(ticker_list, start, end, interval):
+    
+
+#sort_by_sharpe consumes a dataframe of stocks from the same industry and their daily prices prices
 #and produces a sorted dataframe by their sharpe ratio.
 def sort_by_sharpe(dataframe):
   
+def main():
+  ticker_list = ['AAPL', 'SHOP.TO', 'TD.TO', 'MSFT', 'NVDA']
+  start_date = '2019-07-01'
+  end_date = '2024-11-04'
+
